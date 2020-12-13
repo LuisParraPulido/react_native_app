8e2f246abf39e44a3e2ac08472c2a19caef000cb
Luis Alberto Parra Pulido <luis.parra.pulido@gmail.com>
2020-12-13 18:57:40 -0500

    Funcionalidad terminada

--- src/components/notifications/NotificationDetails.js
+++ src/components/notifications/NotificationDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, FlatList, Text, ActivityIndicator, StyleSheet, Pressable } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 const NotificationDetails = ({ navigation, route }) => {
@@ -9,28 +10,52 @@ const NotificationDetails = ({ navigation, route }) => {
   const handleCreate = () => {
     navigation.navigate('create')
   }
-  const handleEdit = () => {
-
+  const handleEdit = async() => {
+    const notificationStr = JSON.stringify(notification);
+    const key = `notification-${route.params.form.title}`;
+    const stored = await Storage.instance.store(key, notificationStr)
+    if(stored) {
+      setEdit(false)
+    }
   }
 
 
   return(
     <View >
-      <View>
-        <Text>{notification.title}</Text>
-      </View>
-      <View>
-        <Text>{notification.description}</Text>
-      </View>
+      {edit ? 
+      <>
+        <View>
+          <TextInput 
+            value={notification.title}
+            onChangeText={(text) => setNotification({...notification, title: text})}
+          />
+        </View>
+        <View>
+          <TextInput 
+            value={notification.description}
+            onChangeText={(text) => setNotification({...notification, title: text})}
+          />
+        </View>
+      </>
+      : 
+      <>
+        <View>
+          <Text>{notification.title}</Text>
+        </View>
+        <View>
+          <Text>{notification.description}</Text>
+        </View>
+      </>
+      }
       <View>
         <Pressable onPress={handleCreate}>
           <Text>
             crear
           </Text>
         </Pressable>
-        <Pressable onPress={handleEdit}>
+        <Pressable onPress={edit ? handleEdit : () => setEdit(true)}>
           <Text>
-            Editar
+            {edit ? 'Save' : 'Edit'}
           </Text>
         </Pressable>
       </View>
--- src/components/notifications/NotificationsScreen.js
+++ src/components/notifications/NotificationsScreen.js
@@ -1,25 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { View, FlatList, Text, TextInput, StyleSheet, Pressable } from 'react-native';
+import Storage from '../../libs/store'
 
 
 const NotificationsScreen = ({ navigation }) => {
   const [notifications, setNotifications] = useState([])
 
   useEffect(() => {
-
+    navigation.addListener('focus', getNotifications)
   }, [])
+
+  const getNotifications = async() => {
+    try {
+      const allKeys = await Storage.instance.getAllkeys();
+  
+      const keys = allKeys.filter((key) => key.includes('notification-'));
+  
+      const notificationStr = await Storage.instance.multiGet(keys);
+  
+      const notifications = notificationStr.map(notification => JSON.parse(notification[1]));
+      console.log(notifications)
+      setNotifications(notifications)
+      
+    } catch (error) {
+      throw Error(error)
+    }
+  }
 
   const handleCreate = () => {
     navigation.navigate('create')
+  }
+
+  const handleNotification = (form) => {
+    navigation.navigate('Details', { form })
   }
 
   return(
     <View >
       {notifications.map(notification => {
         return(
-          <Pressable>
-            <Text>preview</Text>
+          <View>
+          <Text>{notification.title}</Text>
+          <Pressable onPress={() => handleNotification(notification)}>
+            <Text>ver</Text>
           </Pressable>
+          </View>
         )
       })}
       {notifications.length === 0 ?
